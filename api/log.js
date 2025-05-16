7ebce71457e74f1227a750d1400cfd8c85be3464
12345679fl <12345679fl@gmail.com>
2025-05-16 17:36:44 +0200

    Update

--- api/log.js
+++ api/log.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  const { path, timestamp } = req.body
-  console.log(`[LOG] Accesso a ${path} alle ${timestamp}`)
+  const { selectedTime } = req.body
+  console.log(`[LOG] Ha scelto ${selectedTime}`)
   res.status(200).end()
 }
